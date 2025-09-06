b59e69863a4960ec5a49a616176ac0a267a2c1d5
mhmdnab <mhmdnab004@gmail.com>
2025-09-06 23:54:17 +0300

    Code Assessment Frontend v1.2

--- src/utls/api.ts
+++ src/utls/api.ts
@@ -3,7 +3,9 @@ import { Product } from "../types/product";
 
 export async function fetchProducts(category?: string): Promise<Product[]> {
   const query = category ? `?category=${category}` : "";
-  const res = await fetch(`http://localhost:5001/products${query}`);
+  const res = await fetch(
+    `https://code-assessment-backend.onrender.com/products${query}`
+  );
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 }
